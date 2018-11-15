f3e842d2c965d9f7b5c6c3849caa3dac8811d864
karlinglee93 <459607025@qq.com>
2018-11-15 21:16:04 +0800

    update bank5 again

--- src/com/safari/bank/banking/test/TestBanking_5.java
+++ src/com/safari/bank/banking/test/TestBanking_5.java
@@ -14,7 +14,6 @@ public class TestBanking_5 {
         System.out.println("Creating her Savings Account with 500.00 balance and 3% interest.");
         bank.getCustomer(0).setAccount(new CheckingAccount(500));
 
-
         System.out.println("Creating the customer Owen Bryant.");
         bank.addCustomer("Owen", "Bryant");
         System.out.println("Creating his Checking Account with 500.00 balance and no overdraft protection.");
@@ -29,5 +28,31 @@ public class TestBanking_5 {
         bank.addCustomer("Maria", "Soley");
         System.out.println("Maria shares her Checking Account with her husband Tim.");
         bank.getCustomer(3).setAccount(bank.getCustomer(2).getAccount());
+
+        System.out.println("Retrieving the customer Jane Smith with her savings account.");
+        System.out.println("Withdraw 150.00: " + bank.getCustomer(0).getAccount().withdraw(150));
+        System.out.println("Deposit 22.50: " + bank.getCustomer(0).getAccount().deposit(22.50));
+        System.out.println("Withdraw 47.62: " + bank.getCustomer(0).getAccount().withdraw(47.62));
+        System.out.println("Withdraw 400.00: " + bank.getCustomer(0).getAccount().withdraw(400.00));
+        System.out.println("Customer [" + bank.getCustomer(0).getLastName() + ", " + bank.getCustomer(0).getFirstName() + "] has a balance of " + bank.getCustomer(0).getAccount().getBalance());
+
+        System.out.println("Retrieving the customer Owen Bryant with his checking account with no overdraft protection.");
+        System.out.println("Withdraw 150.00: " + bank.getCustomer(1).getAccount().withdraw(150));
+        System.out.println("Deposit 22.50: " + bank.getCustomer(1).getAccount().deposit(22.50));
+        System.out.println("Withdraw 47.62: " + bank.getCustomer(1).getAccount().withdraw(47.62));
+        System.out.println("Withdraw 400.00: " + bank.getCustomer(1).getAccount().withdraw(400.00));
+        System.out.println("Customer [" + bank.getCustomer(1).getLastName() + ", " + bank.getCustomer(1).getFirstName() + "] has a balance of " + bank.getCustomer(1).getAccount().getBalance());
+
+        System.out.println("Retrieving the customer Tim Soley with his checking account that has overdraft protection.");
+        System.out.println("Withdraw 150.00: " + bank.getCustomer(2).getAccount().withdraw(150));
+        System.out.println("Deposit 22.50: " + bank.getCustomer(2).getAccount().deposit(22.50));
+        System.out.println("Withdraw 47.62: " + bank.getCustomer(2).getAccount().withdraw(47.62));
+        System.out.println("Withdraw 400.00: " + bank.getCustomer(2).getAccount().withdraw(400.00));
+        System.out.println("Customer [" + bank.getCustomer(2).getLastName() + ", " + bank.getCustomer(2).getFirstName() + "] has a balance of " + bank.getCustomer(2).getAccount().getBalance());
+
+        System.out.println("Retrieving the customer Maria Soley with her joint checking account with husband Tim.");
+        System.out.println("Deposit 150: " + bank.getCustomer(3).getAccount().deposit(150));
+        System.out.println("Withdraw 750.00: " + bank.getCustomer(3).getAccount().withdraw(750));
+        System.out.println("Customer [" + bank.getCustomer(3).getLastName() + ", " + bank.getCustomer(3).getFirstName() + "] has a balance of " + bank.getCustomer(3).getAccount().getBalance());
     }
 }
