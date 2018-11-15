792f3befb34bdca27eda3ef0adc4f59efa84b5a3
karlinglee93 <459607025@qq.com>
2018-11-15 15:34:52 +0800

    bank 2

---
+++ src/com/safari/bank/banking/TestBanking_2.java
@@ -0,0 +1,22 @@
+package com.safari.bank.banking;
+
+public class TestBanking_2 {
+    public static void main(String[] args) {
+        System.out.println("Creating the customer Jane Smith");
+        Customer customer = new Customer("Jane", "Smith");
+
+        System.out.println("Creating her account with a 500.00 balance");
+        customer.setAccount(new Account(500));
+
+        System.out.println("Withdraw 150.00");
+        customer.getAccount().withdraw(150);
+
+        System.out.println("Deposit 22.50");
+        customer.getAccount().deposit(22.50);
+
+        System.out.println("Withdraw 47.62");
+        customer.getAccount().withdraw(47.62);
+
+        System.out.println("Customer [" + customer.getLastName() + ", " + customer.getFirstName() + "] has a balance of " + customer.getAccount().getBalance());
+    }
+}
